9c1dec6695cd3566dd3017c2c9d35beac7902daa
Replit user <>
2023-06-26 15:30:48 +0000

    Basic Intro to Python Commands

---
+++ session_01/lesson_1.py
@@ -0,0 +1,24 @@
+#Section A
+
+print("Hello World")
+
+print("1 2 3 4 5")
+
+print("Hello\nWorld")
+
+print("My List of Names:\n\tAlice\n\tBob\n\tCharlie")
+
+#Section B
+#Better way is to store in variables, then print the variable
+print(2 + 2)
+
+print(3.4 - 5.7)
+
+print(8*7)
+
+print(144/12)
+
+print(67%12)
+
+print(((4 - 2) * 6 / 3) * 5)
+
